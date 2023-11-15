efbfe47d71cfb21b07152e2a12c0eba9e38a86d4
Alan <cxy814941789@gmail.com>
2023-11-15 18:49:35 +0800

    feat: websocket demo

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 @EnableCaching //开启缓存
+@EnableScheduling // 开启定时任务
 public class SkyApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SkyApplication.class, args);
